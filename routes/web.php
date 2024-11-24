96296c644581e64adb532fbea136aefaefe6cbc9
Zouine2005 <zouinemohamade@gmail.com>
2024-11-24 14:16:57 +0100

    routes folder

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmployerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login',[AuthController::class, 'login'])->name('login');
+Route::post('/login',[AuthController::class, 'handleLogin'])->name('handlelogin');
+
+//Route securise
+Route::middleware('auth')->group(function(){
+    Route::get('dashboard',[AppController::class, 'show'])->name('dashboard');
+
+    Route::prefix('employers')->group (function () {
+        Route::get("/", [EmployerController::class, 'index'])->name('employer.index');
+        Route::get('/create', [EmployerController::class, 'create'])->name ('employer.create');
+        Route::get('/edit/{employer}', [EmployerController::class, 'edit'])->name('employer.edit');
+        });
+
+});
+
