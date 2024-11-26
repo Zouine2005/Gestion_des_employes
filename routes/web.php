784df7d925ecb4396a8e10e6c222101a8d24f017
Zouine2005 <zouinemohamade@gmail.com>
2024-11-27 00:12:51 +0100

    add some new routes for departement

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EmployerController;
+use App\Http\Controllers\DepartementController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,6 +32,13 @@ Route::middleware('auth')->group(function(){
         Route::get('/create', [EmployerController::class, 'create'])->name ('employer.create');
         Route::get('/edit/{employer}', [EmployerController::class, 'edit'])->name('employer.edit');
         });
+    
+        Route::prefix('departement')->group(function() {
+            Route::get('/', [DepartementController::class, 'index'])->name('departement.index');
+            Route::get('/create', [DepartementController::class, 'create'])->name('departement.create');
+            Route::post('/create', [DepartementController::class, 'store'])->name('departement.store');
+            Route::get('/edit/{departement}', [DepartementController::class, 'edit'])->name('departement.edit');
+            });
 
 });
 
