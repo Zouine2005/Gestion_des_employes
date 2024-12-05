cc62db5c3fbe62ed14db5ec757012b4d6b1caa25
Zouine2005 <zouinemohamade@gmail.com>
2024-12-05 21:50:25 +0100

    add count payment

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -45,8 +45,8 @@
     <div class="col-6 col-lg-3">
         <div class="app-card app-card-stat shadow-sm h-100">
             <div class="app-card-body p-3 p-lg-4">
-                <h4 class="stats-type mb-1">Retard de paiement</h4>
-                <div class="stats-figure">0</div>
+                <h4 class="stats-type mb-1">Paiements</h4>
+                <div class="stats-figure">{{$totalPayemnts}}</div>
             </div><!--//app-card-body-->
             <a class="app-card-link-mask" href="#"></a>
         </div><!--//app-card-->
