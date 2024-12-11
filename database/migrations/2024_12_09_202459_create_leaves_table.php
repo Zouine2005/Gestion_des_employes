84e4cd925b02aa433d7ca650e3282e531ca134f1
Zouine2005 <zouinemohamade@gmail.com>
2024-12-11 01:09:35 +0100

    new migration

--- database/migrations/2024_12_09_202459_create_leaves_table.php
+++ database/migrations/2024_12_09_202459_create_leaves_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('leaves', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->foreign('employee_id')->references('id')->on('employers')->onDelete('cascade');
+            $table->date('start_date')->comment('Date de début du congé');
+            $table->date('end_date')->comment('Date de fin du congé');
+            $table->enum('status', ['EN ATTENTE', 'APPROUVÉ', 'REJETÉ'])->default('EN ATTENTE');
             $table->timestamps();
         });
     }
